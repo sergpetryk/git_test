adb280e8511e60981641005f37f5611ef1dae0f9
Serhii Petryk <sergpetryk@gmail.com>
2022-05-30 17:19:58 +0300

    First commit

---
+++ test.py
@@ -0,0 +1,28 @@
+a = 37
+if a == 0:
+    print('зеленый')
+if 0 < a <=10:
+    if a % 2 == 0:
+        print('красный')
+    else:
+        print('черный')
+if 10 < a <= 18:
+    if a % 2 == 0:
+        print('черный')
+    else:
+        print('красный')
+if 19 < a <= 28:
+    if a % 2 == 0:
+        print('черный')
+    else:
+        print('красный')
+if 29 < a <= 36:
+    if a % 2 == 0:
+        print('черный')
+    else:
+        print('красный')
+if 36 < a  or a < 0:
+    print('ошибка ввода')
+
+
+
