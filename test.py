2ac6d6efcc91cacf33e156c715a421bac3b5931c
Serhii Petryk <sergpetryk@gmail.com>
2022-06-01 14:15:01 +0300

    Hello World

--- test.py
+++ test.py
@@ -1,28 +1,3 @@
-a = 37
-if a == 0:
-    print('зеленый')
-if 0 < a <=10:
-    if a % 2 == 0:
-        print('красный')
-    else:
-        print('черный')
-if 10 < a <= 18:
-    if a % 2 == 0:
-        print('черный')
-    else:
-        print('красный')
-if 19 < a <= 28:
-    if a % 2 == 0:
-        print('черный')
-    else:
-        print('красный')
-if 29 < a <= 36:
-    if a % 2 == 0:
-        print('черный')
-    else:
-        print('красный')
-if 36 < a  or a < 0:
-    print('ошибка ввода')
-
+print("Hello, World!")
 
 
